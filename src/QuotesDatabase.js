404860e1ee34d96eb647fe91bd457d41259da254
BhuvaneswariKannan <kbhuvaneswari48@gmail.com>
2020-07-02 10:43:18 +0530

    First commit message

---
+++ src/QuotesDatabase.js
@@ -0,0 +1,44 @@
+const quotes=[
+    {
+        "quote": "The way to get started is to quit talking and begin doing.",
+        "author": "WaltDisney"
+    },
+    {
+        "quote": "In your way to the diamonds, you will find much of golds, dont get distracted.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "People arguing that god is just a statue,are not going to accept money as a paper.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Not only failures are stepping stones, sometimes previous successes do inspire.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "When dreams are too big, there exists no competition.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Success is no accident.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Moving on is totally different from giving up.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Finally you have only you.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Healing is a time process.",
+        "author": "AashiBhuvana"
+    },
+    {
+        "quote": "Being busy is different from being productive.",
+        "author": "AashiBhuvana"
+    }
+]
+
+export default quotes;
